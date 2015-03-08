d39d98b09431b10a614bc774f05cf2e2155d2d99
Steve Lacey <steve@stevelacey.net>
2015-03-08 21:14:31 +0000

    Ghost ESI example

---
+++ src/WordPressBundle/Controller/GhostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WordPressBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/spooky")
+ */
+class GhostController extends Controller
+{
+    /**
+     * @Route("/", name="ghost")
+     * @Route("/{x}", requirements={"x": ".+"}, name="ghost_page")
+     * @Template
+     */
+    public function indexAction(Request $request)
+    {
+        return array(
+            'uri' => 'http://blog.steve' . $request->getRequestUri()
+        );
+    }
+}
